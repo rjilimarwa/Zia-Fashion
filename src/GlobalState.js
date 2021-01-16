936c286f65a3785993e31fa36b21d538ce7565e2
rjilimarwadjerba@gmail.com <admin@exemple.com>
2021-01-16 20:42:05 +0000

    add  list Products

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -1,9 +1,14 @@
 import React ,{createContext,useState} from 'react';
-
+import ProductsAPI from './api/ProductsAPI'
 
 export const GlobalState = createContext();
 export const DataProvider = ({children})=>{
-  return (<GlobalState.Provider value={children}>
+    const [token,setToken]=useState(false);
+    const state={
+        token: [token,setToken],
+        ProductsAPI:ProductsAPI()
+    }
+  return (<GlobalState.Provider value={state}>
         {children}
         </GlobalState.Provider>
 );
---
+++ src/api/ProductsAPI.js
@@ -0,0 +1,20 @@
+import React,{useState,useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios'
+function ProductsApi (){
+    const [products,setProducts]=useState([]);
+    const getProducts=async()=>{
+        const res= await axios.get('/api/products')
+        setProducts(res.data.products)
+    }
+    useEffect(()=>{
+      getProducts()
+    },[])
+
+    return(
+      products:[products,setProducts]
+
+    )
+}
+
+export default ProductsApi;
