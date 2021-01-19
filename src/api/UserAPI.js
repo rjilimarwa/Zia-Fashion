89a509ddaf343bc98ac2574406f9abaacd249482
rjilimarwadjerba@gmail.com <admin@exemple.com>
2021-01-19 19:52:50 +0000

    add  product to cart

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 function UserAPI(token) {
     const [isLogged, setIsLogged] = useState(false)
     const [isAdmin, setIsAdmin] = useState(false)
+    const [cart, setCart] = useState([])
 
     useEffect(() =>{
         if(token){
@@ -16,6 +17,8 @@ function UserAPI(token) {
                     setIsLogged(true)
                     res.data.role === 1 ? setIsAdmin(true) : setIsAdmin(false)
 
+                    setCart(res.data.cart)
+
                 } catch (err) {
                     alert(err.response.data.msg)
                 }
@@ -26,9 +29,32 @@ function UserAPI(token) {
         }
     },[token])
 
+
+
+    const addCart = async (product) => {
+        if(!isLogged) return alert("Please login to continue buying")
+
+        const check = cart.every(item =>{
+                return item._id !== product._id
+            })
+
+        if(check){
+            setCart([...cart, {...product, quantity: 1}])
+
+            await axios.patch('/user/addcart', {cart: [...cart, {...product, quantity: 1}]}, {
+                headers: {Authorization: token}
+            })
+
+        }else{
+            alert("This product has been added to cart.")
+        }
+    }
+
     return {
         isLogged: [isLogged, setIsLogged],
-        isAdmin: [isAdmin, setIsAdmin]
+        isAdmin: [isAdmin, setIsAdmin],
+        cart: [cart, setCart],
+        addCart: addCart
     }
 }
 
