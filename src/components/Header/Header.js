99a4959208151e834a72b8724d454355d225689f
rjilimarwadjerba@gmail.com <admin@exemple.com>
2021-01-19 08:29:05 +0000

    change page headers to make choice between Users or Admin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,87 @@
-import React from 'react';
-import  './Header.css';
-import Menu from './icon/Menu.svg';
-import Cart from './icon/Cart.svg';
-import Close from './icon/Close.svg';
-import {Link} from 'react-router-dom';
+import React, {useContext, useState} from 'react'
+import {GlobalState} from '../../GlobalState'
+import Menu from './icon/menu.svg'
+import Close from './icon/close.svg'
+import Cart from './icon/cart.svg'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
 
-function Header (){
-    return(
-        <Header>
-         <div className="menu">
-            <img src={Menu} alt="" width="30"/>
-            </div>
-            <div className="logo">
-            <h1><Link to="/">Zia Fashion</Link> </h1>
-            </div>
-            <ul>
-             <li><Link to="/products">Products</Link></li>
-              <li><Link to="/login">Login *Register</Link></li>
-              <li><img src={Close} alt="" width="30"/></li>
-            </ul>
-            <div className="cart-icon">
-            <span>0</span>
-            <Link to="/cart"><img src={Cart} alt="" width="30"/></Link>
-            </div>
-        </Header>
+function Header() {
+    const state = useContext(GlobalState)
+    const [isLogged] = state.userAPI.isLogged
+    const [isAdmin] = state.userAPI.isAdmin
+    const [menu, setMenu] = useState(false)
+
+    const logoutUser = async () =>{
+        await axios.get('/user/logout')
+
+        localStorage.removeItem('firstLogin')
+
+        window.location.href = "/";
+    }
+
+    const adminRouter = () =>{
+        return(
+            <>
+            <li><Link to="/create_product">Create Product</Link></li>
+        <li><Link to="/category">Categories</Link></li>
+            </>
     )
+    }
+
+    const loggedRouter = () =>{
+        return(
+            <>
+            <li><Link to="/history">History</Link></li>
+            <li><Link to="/" onClick={logoutUser}>Logout</Link></li>
+            </>
+    )
+    }
+
+
+    const styleMenu = {
+        left: menu ? 0 : "-100%"
+    }
+
+    return (
+        <header>
+        <div className="menu" onClick={() => setMenu(!menu)}>
+<img src={Menu} alt="" width="30" />
+        </div>
+
+        <div className="logo">
+        <h1>
+        <Link to="/">{isAdmin ? 'Admin' : 'DevAT Shop'}</Link>
+        </h1>
+        </div>
+
+        <ul style={styleMenu}>
+        <li><Link to="/">{isAdmin ? 'Products' : 'Shop'}</Link></li>
+
+        {isAdmin && adminRouter()}
+
+    {
+        isLogged ? loggedRouter() : <li><Link to="/login">Login ✥ Register</Link></li>
+    }
+
+<li onClick={() => setMenu(!menu)}>
+<img src={Close} alt="" width="30" className="menu" />
+        </li>
+
+        </ul>
+
+        {
+            isAdmin ? ''
+                :<div className="cart-icon">
+        <span>{cart.length}</span>
+    <Link to="/cart">
+        <img src={Cart} alt="" width="30" />
+        </Link>
+        </div>
+}
+
+</header>
+)
 }
 
 export default Header
